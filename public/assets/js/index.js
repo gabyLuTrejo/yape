ed0534560e9bc85d532921a7828cf83a15c4037c
gabytrejo <gabytrejo303@gmail.com>
2017-07-19 12:35:25 +0000

    Third page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,15 +1,25 @@
 console.log("js gulp-watch");
 
+const api = {
+    url: 'http://localhost:3000/api/registerNumber'
+};
+
 const loadPage = function(){
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
-	$('#btn-init').click(nextPage);
+	$('#btn-init').click(nextPage1);
+	$('#btn-next').click(nextPage2);
 	$("#formPhone").submit(validate);
 	$("#phoneNumber").keyup(digits);
 };
 
-const nextPage = function(){
+const nextPage1 = function(){
 	console.log("next");
 	window.location.href="views/validate.html";
+}
+
+const nextPage2 = function(){
+	console.log("next");
+	window.location.href="code.html";
 }
 
 const validate = function(e){
@@ -31,5 +41,6 @@ const termsValidate = function(){
 		$("#btn-next")[0].disabled = true;
 	}
 }
+
 
 $(document).ready(loadPage);
