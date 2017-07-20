d85f62e9ac16f3cd4c92e6d264b2ae86105e2e62
gabytrejo <gabytrejo303@gmail.com>
2017-07-20 12:29:23 +0000

    3s transition from page5 to page6

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -18,16 +18,35 @@ const loadPage = function(){
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
 	$('#btn-init').click(nextPage1);
 	$('#btn-next').click(nextPage2);
+	$('#btn-next2').click(nextPage5);
 	$("#formPhone").submit(validate);
+	$("#formCode").submit(validate);
 	$("#phoneNumber").keyup(digits);
 };
 
 const nextPage1 = function(){
 	console.log("next");
 	window.location.href="views/validate.html";
-}
+};
 
 const nextPage2 = function(){
+	console.log("next");
+	$.post(api.user,{
+		"phone": $("#phoneNumber").val(),
+		"terms": !($("#btn-next")[0].disabled)
+	}).then(function (response){
+		console.log(response.data);
+		alert("Código de Validación: " + response.data.code);
+		registerData(response.data);
+		window.location.href = "code.html ";
+	}).catch(function (error) {
+        console.log(error);
+        alert("Teléfono no válido, ya está registrado");
+    })	
+	
+};
+
+const nextPage5 = function(){
 	console.log("next");
 	$.post(api.url,{
 		"phone": $("#phoneNumber").val(),
@@ -42,18 +61,19 @@ const nextPage2 = function(){
         alert("Teléfono no válido, ya está registrado");
     })	
 	
-}
+};
+
 
 const validate = function(e){
 	e.preventDefault();
-}
+};
 
 const digits = function(){
 	var digitNumbers = $("#phoneNumber").val().length;	
 	if (digitNumbers == 10) {
 		$("#term-conditions").click(termsValidate);	
 	}
-}
+};
 
 const termsValidate = function(){
 	var terms = $("#btn-next")[0].disabled;
@@ -62,6 +82,18 @@ const termsValidate = function(){
 	} else {
 		$("#btn-next")[0].disabled = true;
 	}
-}
+};
+
+const userData = function(){
+	var userName = $("#userName").required;
+	var userMail = $("#userMail").required;
+	var userPass = $("#userPass").required;
+	if (userName && userMail && userPass) {
+		$("#btn-next2")[0].disabled = false;
+	} else {}
+};
+
+
+
 
 $(document).ready(loadPage);
---
+++ public/assets/js/success.js
@@ -0,0 +1,5 @@
+const nextPage6 = function (){
+    setTimeout(function(){window.location.href="card.html"},5000)
+};
+
+$(document).ready(nextPage6);
